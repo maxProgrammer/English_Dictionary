15c3cc1f4c508c931fa66fd0bfeacb9d01981e74
maxProgrammer <maxwneto.poo@gmail.com>
2021-08-04 14:07:42 -0300

    adicionado check para condiçoes de caixa baixa, caixa alta e capitalize

--- app1.py
+++ app1.py
@@ -1,20 +1,25 @@
 import json
 from difflib import get_close_matches
 
-
-
 data = json.load(open("data.json"))
 
+
 def translate(word):
-    word = word.lower()
-    if word in  data:
-        return data[word]
-    
-    #se houve digitação retorna sugere outra palavra compatível ao usuário
-    elif len(get_close_matches(word,data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter y if yes, or N if no: " % get_close_matches(word,data.keys())[0])
+    win = word.lower()
+    if win in data:
+        return data[win]
+
+    elif win.capitalize() in data:
+        return data[win.capitalize()]
+
+    elif win.upper() in data:
+        return data[win.upper()]
+
+    # se houve digitação retorna sugere outra palavra compatível ao usuário
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
         if yn.capitalize() == "Y":
-            return data[get_close_matches(word,data.keys())[0]]
+            return data[get_close_matches(word, data.keys())[0]]
 
         elif yn.capitalize() == "N":
             return "Word does not exist. Please double ckeck it."
@@ -22,14 +27,13 @@ def translate(word):
         else:
             return "We didn't understand your entry."
 
-
     else:
         return "Word does not exist. Please double ckeck it."
 
-word = input("Enter word:")
 
-output = translate(word)
-#se é uma lista imprime por linha/indice
+w = input("Enter word:")
+output = translate(w)
+# se é uma lista imprime por linha/indice
 if type(output) == list:
     for item in output:
         print(item)
