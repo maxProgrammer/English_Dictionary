88bb17772805c0adf5353bad3ea9f62488be062a
maxProgrammer <maxwneto.poo@gmail.com>
2021-08-03 23:35:54 -0300

    criado tratamento onde oferece uma palavra compativel com a digitada quando a mesma nao e encontrada no dicionario.

--- app1.py
+++ app1.py
@@ -10,13 +10,29 @@ def translate(word):
     if word in  data:
         return data[word]
     
-    #se houve digitação retorna a sugestão de uma palavra compatível
+    #se houve digitação retorna sugere outra palavra compatível ao usuário
     elif len(get_close_matches(word,data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(word,data.keys())[0]
+        yn = input("Did you mean %s instead? Enter y if yes, or N if no: " % get_close_matches(word,data.keys())[0])
+        if yn.capitalize() == "Y":
+            return data[get_close_matches(word,data.keys())[0]]
+
+        elif yn.capitalize() == "N":
+            return "Word does not exist. Please double ckeck it."
+
+        else:
+            return "We didn't understand your entry."
+
 
     else:
         return "Word does not exist. Please double ckeck it."
 
 word = input("Enter word:")
 
-print(translate(word))
+output = translate(word)
+#se é uma lista imprime por linha/indice
+if type(output) == list:
+    for item in output:
+        print(item)
+
+else:
+    print(output)
